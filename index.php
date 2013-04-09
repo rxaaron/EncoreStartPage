d1084e7492e115093e1926b45f648df1e35480b9
ataylor <ataylor@GMAP_RCB_2.grcs.local>
2013-04-09 14:19:55 -0400

    move to php

---
+++ index.php
@@ -0,0 +1,39 @@
+<!--
+To change this template, choose Tools | Templates
+and open the template in the editor.
+-->
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Encore Pharmacy</title>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <link rel="stylesheet" href="rsc/esp.css" type="text/css" />
+        <link rel="shortcut icon" href="rsc/favicon.ico" />
+        <script>
+            function navigate(inpt){
+                document.location.href= inpt;
+            }
+        </script>
+    </head>
+    <body>
+        <div id="banner">
+            <center>
+            <img src="rsc/ENC_Logo.png" alt="Encore!!!" width ="250" height="100">
+            </center>
+        </div>
+        <div id="nav">
+            <div id="cs" class="c2" onclick="navigate('narclog/');">
+                <p>C II Log</p>
+            </div>
+            <div id="er" class="erbox" onclick="navigate('erbox/');">
+                <p>ER Box</p>
+            </div>
+            <div id="pais" class="pais" onclick="navigate('pais/');">
+                <p>PAIS Bulk</p>
+            </div>
+            <div id="sfc" class="sfcform" onclick="navigate('sfc/');">
+                <p>SFC Formulary</p>
+            </div>
+        </div>
+    </body>
+</html>
