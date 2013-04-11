a4eb14b0bfe309c95926882a54b6150bba1bf5ce
ataylor <ataylor@GMAP_RCB_2.grcs.local>
2013-04-11 14:08:04 -0400

    nav increase

--- index.php
+++ index.php
@@ -21,17 +21,29 @@ and open the template in the editor.
         </div>
         <div id="main">
             <div id="nav">
+                <div id="wiki" class="wiki" onclick="navigate('wiki/');">
+                    <p>Wiki</p>
+                </div>
                 <div id="cs" class="c2" onclick="navigate('narclog/');">
                     <p>C II Log</p>
                 </div>
+                <div id="calendar" class="calendar" onclick="navigate('calendar/');">
+                    <p>Calendar</p>
+                </div>
                 <div id="er" class="erbox" onclick="navigate('erbox/');">
                     <p>ER Box</p>
+                </div>
+                <div id="manifests" class="manifests" onclick="navigate('manifests/');">
+                    <p>Manifests</p>
                 </div>
                 <div id="pais" class="pais" onclick="navigate('pais/');">
                     <p>PAIS Bulk</p>
                 </div>
                 <div id="sfc" class="sfcform" onclick="navigate('sfc/');">
                     <p>SFC Formulary</p>
+                </div>
+                <div id="documents" class="documents" onclick="navigate('docs/');">
+                    <p>Document Manager</p>
                 </div>
             </div>
         </div>
